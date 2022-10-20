7c25fd04fe38c70ed975b30b199beb3fb01d8cf5
Nikhil <ndeepak@ucsd.edu>
2022-10-20 14:59:41 -0700

    Update count-txts.sh

--- count-txts.sh
+++ count-txts.sh
@@ -1,3 +1,3 @@
-find technical > find-results.txt
-grep ".txt" find-results.txt > grep-results.txt
-wc grep-results.txt
+find $1 > find-results.txt
+grep ".txt" find-results.txt > grep-results2.txt
+wc grep-results2.txt
